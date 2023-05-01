56d786904e04aa87db6837a4e1735445bcc13d42
Adam <96756923+ramblingadam@users.noreply.github.com>
2023-04-30 20:18:38 -0500

    reinit with next and tailwind

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <header className='header-wrapper flex flex-row p-4'>
+      <h1 className="text-3xl"><span className="font-bold">DSA</span>mazing</h1>
+    </header>
+  )
+}
+
+export default Header
