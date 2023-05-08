e8837f954077302df6bc2913a56b4202fe1ebea2
Adam <96756923+ramblingadam@users.noreply.github.com>
2023-05-08 00:35:46 -0500

    refine stylgin of side bars

--- components/DSMain.tsx
+++ components/DSMain.tsx
@@ -22,7 +22,7 @@ const DSMain = () => {
   return (
     <main className='ds-main-wrapper flex flex-row flex-1 w-full h-full'>
       {/* //// DS DETAILS SIDEBAR (wide screen only)- Shows text representing current data strucutre, or current node if node selected */}
-      <section className='ds-side-details-wrapper md:lg:w-40 md:lg:block hidden bg-gray-900 border-r-2 border-black'>
+      <section className='ds-side-details-wrapper md:lg:max-w-xs md:lg:min-w-[15rem]  md:lg:block hidden hidden bg-gray-900 border-r-2 border-black'>
         HEY YOU
       </section>
       {/* //// MAIN CENTRAL DISPLAY */}
@@ -33,7 +33,7 @@ const DSMain = () => {
       </section>
       {/* //// EVENT LOG SIDEBAR (wide screen only) */}
       {/* <section className='ds-eventlog-wrapper w-40'></section> */}
-      <section className='ds-side-eventlog-wrapper md:lg:w-40 md:lg:block hidden bg-gray-900 border-l-2 border-black'>
+      <section className='ds-side-eventlog-wrapper md:lg:max-w-xs md:lg:min-w-[15rem]  md:lg:block hidden bg-gray-900 border-l-2 border-black'>
         HEY YOU
       </section>
     </main>
